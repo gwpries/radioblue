5274d2156ca4990659ad3d956a17000e3a3d2180
Greg Pries <gpries@Gregs-MacBook-Pro.local>
2023-10-23 22:35:52 -0400

    woot

--- nowplaying.star
+++ nowplaying.star
@@ -17,15 +17,26 @@ def main():
     track_left_color = rep.json()['track_left_color']
     queue_count = rep.json()['queue_count']
     queue_color = rep.json()['queue_color']
+    silence = rep.json()['silence']
  
     percent = rep.json()['percent'] 
 
+    text_rows = [
+        render.Text("%s" % track_title, color=track_title_color),
+        render.Text("C: %s:%s (%d%%)" % (minutes, seconds, percent), color=track_left_color),
+        render.Text("Q: %s|%s:%s:%s" % (queue_count, td_hours, td_minutes, td_seconds), color=queue_color)
+    ]
+
+    if silence:
+        if silence == "queued":
+            text_rows.append(render.Text("MIC QUEUED"))
+        elif silence == "next":
+            text_rows.append(render.Text("SILENCE NEXT"))
+        elif silence == "now":
+            text_rows.append(render.Text("SPEAK NOW"))
+ 
     return render.Root(
         child = render.Column(
-            children = [
-                render.Text("%s" % track_title, color=track_title_color),
-                render.Text("C: %s:%s (%d%%)" % (minutes, seconds, percent), color=track_left_color),
-                render.Text("Q: %s|%s:%s:%s" % (queue_count, td_hours, td_minutes, td_seconds), color=queue_color)
-            ]
+            children = text_rows
         )
     )
--- radiobluequeue.py
+++ radiobluequeue.py
@@ -8,6 +8,12 @@ import json
 import logging
 import requests
 import base64
+import threading
+import subprocess
+import signal
+
+from flask import Flask, jsonify
+app = Flask(__name__)
 
 from InquirerPy import inquirer
 from InquirerPy.base import Choice
@@ -25,12 +31,14 @@ logging.getLogger("__main__").setLevel(logging.DEBUG)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("charset_normalizer").setLevel(logging.WARNING)
 logging.getLogger("asyncio").setLevel(logging.WARNING)
+logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
 
 SERVER_URL = 'http://127.0.0.1:32400'
 SERVER_TOKEN = os.getenv('PLEX_TOKEN', '')
 CLIENT_NAME = os.getenv('CLIENT_NAME', 'MyPlexamp')
 LIBRARY_SECTION = os.getenv('LIBRARY_SECTION', 'Music')
+PIXLET_PATH = '/usr/local/bin/pixlet'
 CONFIG_FILE = 'config.json'
 NOW_PLAYING = """
 Title: {title}
@@ -48,8 +56,11 @@ class RadioBlueQueue:
         self.options = {}
         self.play_queue = None
         self.client = None
+        self.queued_songs = {}
         self.played_songs = {}
         self.currently_playing = {}
+        self.playing_next = {}
+        self.ready = False
 
     def setup(self):
         self.options = self.get_all_options()
@@ -62,13 +73,16 @@ class RadioBlueQueue:
         """Load all playlists"""
         output = {}
         output['on-air'] = self.server.playlist(self.options.get('on_air_playlist'))
-        LOG.debug("Playlists loaded")
-        LOG.debug(output)
         return output
 
     def init_play_queue(self, items):
+        music_section = self.server.library.section("Music")
+        if self.options.get('silence_track'):
+            for track in music_section.searchTracks(guid=self.options.get('silence_track')):
+                silence_track = track
+            items = [silence_track] + items
         for item in items:
-            self.played_songs[item.title] = item
+            self.queued_songs[item.guid] = item
         return PlayQueue.create(self.server, items)
 
     def play(self):
@@ -86,6 +100,8 @@ class RadioBlueQueue:
             if confirm:
                 self.options = prev_options
                 self.test_server_connection()
+                playlist_options = self.get_playlist_options(prev_options)
+                self.options.update(playlist_options)
                 return prev_options
 
         options = self.get_connection_options(prev_options)
@@ -166,6 +182,23 @@ class RadioBlueQueue:
             default=old_options.get('client_name'),
             choices=client_options
         ).execute()
+        options['pixlet_path'] = inquirer.text(
+            message="If you have a tidbyt, point to pixlet binary",
+            default=old_options.get('pixlet_path', PIXLET_PATH)
+        ).execute()
+        if os.path.exists(options['pixlet_path']):
+            options['tidbyt_api_token'] = inquirer.text(
+                message="Tidbyt API Token",
+                default=old_options.get('tidbyt_api_token', '')
+            ).execute()
+            options['tidbyt_device_id'] = inquirer.text(
+                message="Tidbyt Device ID",
+                default=old_options.get('tidbyt_device_id', '')
+            ).execute()
+        options['silence_track'] = inquirer.text(
+            message="Silence track GUID (Optional)",
+            default=old_options.get('silence_track', '')
+        ).execute()
         return options
 
     def get_connection_options(self, old_options):
@@ -231,6 +264,10 @@ class RadioBlueQueue:
         """Fetch the plex client"""
         return self.server.client(self.options['client_name'])
 
+    def refresh_play_queue_from_server(self):
+        """Pull the PQ from the server"""
+        self.play_queue = self.play_queue.get(self.server, self.play_queue.playQueueID)
+
     def refresh_play_queue(self):
         """Refresh the play queue"""
         self.client.refreshPlayQueue(self.play_queue)
@@ -239,11 +276,11 @@ class RadioBlueQueue:
         """Sync the play queue and play list"""
         playlists = self.load_playlists()
         for song in playlists.get('on-air'):
-            if song.title in self.played_songs:
+            if song.guid in self.queued_songs:
                 #LOG.debug(f'{song.title} has already been played')
                 continue
             LOG.debug(f'Adding {song.title} to queue')
-            self.played_songs[song.title] = song
+            self.queued_songs[song.guid] = song
             self.play_queue.addItem(song)
             self.refresh_play_queue()
 
@@ -263,10 +300,8 @@ class RadioBlueQueue:
         for session in self.server.sessions():
             if session.player.title != self.options['client_name']:
                 continue
-            #if self.currently_playing.get('title') == session.title:
-            #    continue
-            LOG.debug(session.player.title)
-            LOG.debug(session.parentTitle)
+            if self.currently_playing.get('title') == session.title:
+                continue
             ms = session.duration
             seconds, ms = divmod(ms, 1000)
             minutes, seconds = divmod(seconds, 60)
@@ -281,6 +316,20 @@ class RadioBlueQueue:
             self.currently_playing = {
                 'title': session.title,
             }
+            ps_key = session.guid
+            self.played_songs[ps_key] = True
+
+            next_track = False
+            for item in self.play_queue:
+                if item.guid == session.guid:
+                    next_track = True
+                    continue
+                if next_track:
+                    self.playing_next = {
+                        'title': item.title,
+                        'guid': item.guid
+                    }
+                    break
 
             now_playing_txt = NOW_PLAYING.format(
                 title=session.title,
@@ -292,30 +341,194 @@ class RadioBlueQueue:
             with open('./Now Playing.txt', 'w', encoding='utf-8') as out_fh:
                 out_fh.write(now_playing_txt)
 
-    def update_time_remaining(self):
+    def update_stats(self):
         """Update time remaining"""
+        track_title = ''
+        if self.currently_playing:
+            track_title = self.currently_playing.get('title')
+
+        total_duration = 0
+        queue_count = 0 
+        silence = ""
+        for item in self.play_queue.items:
+            if self.currently_playing and self.currently_playing.get('title') == item.title:
+                if item.guid == self.options.get('silence_track'):
+                    silence = 'now'
+                total_duration += item.duration
+            if self.played_songs.get(item.guid):
+                continue
+            if track_title != item.title:
+                queue_count += 1 
+            if not silence and item.guid == self.options.get('silence_track'):
+                silence = "queued"
+            total_duration += item.duration
+        if self.playing_next and self.playing_next.get('guid') == self.options.get('silence_track'):
+            silence = "next"
+        td_seconds = int((total_duration / 1000) % 60)
+        td_minutes = int((total_duration / (1000 * 60)) % 60)
+        td_hours = int((total_duration / (1000 * 60 * 60)) % 60)
+
+        mediatype = None
         for mediatype in self.client.timelines():
             if not mediatype.time:
                 continue
-            curtime = mediatype.time
-            curdur = mediatype.duration
-            curtimemin = int(curtime)/60000
-            curdurmin = int(curdur)/60000
-            percent = int((curtimemin / curdurmin) * 100)
-            LOG.debug(f"We are at minute {curtimemin} out of {curdurmin} ({percent}%)")
+            if not mediatype.duration:
+                continue
+            break
+        curtime = mediatype.time
+        curdur = mediatype.duration
+        if not curtime or not curdur:
+            return
+        timeleft = curdur - curtime
+        if not timeleft:
+            return
+        if timeleft == 'NaN':
+            return
+        millis = int(timeleft)
+        seconds=(millis/1000)%60
+        seconds = int(seconds)
+        minutes=(millis/(1000*60))%60
+        minutes = int(minutes)
+        hours=(millis/(1000*60*60))%24
+        curtimemin = int(curtime)/60000
+        curdurmin = int(curdur)/60000
+        percent = int((curtimemin / curdurmin) * 100)
+
+        total_duration -= curtime
+        td_seconds = int((total_duration / 1000) % 60)
+        td_minutes = int((total_duration / (1000 * 60)) % 60)
+        td_hours = int((total_duration / (1000 * 60 * 60)) % 60)
+
+        if timeleft < 30000:
+            track_left_color = "#ff0000"
+        elif timeleft < 60000:
+            track_left_color = "#ff9500"
+        else:
+            track_left_color = "#00ff00"
+
+        queue_color = '#0000ff'
+        if queue_count < 1:
+            queue_color = "#ff0000"
+        elif queue_count < 2:
+            queue_color = "#ff9500"
+        else:
+            queue_color = "#00ff00"
+
+        timeleft = {
+            'hours': hours,
+            'minutes': f'{minutes:02}',
+            'seconds': f'{seconds:02}',
+            'td_hours': f'{td_hours:02}',
+            'td_minutes': f'{td_minutes:02}',
+            'td_seconds': f'{td_seconds:02}',
+            'queue_count': f'{queue_count:02}',
+            'queue_color': queue_color, 
+            'track_left_color': track_left_color,
+            'track_title': track_title,
+            'percent': percent,
+            'silence': silence
+        }
+        with open('./timeleft.json', 'w', encoding='utf-8') as tl_fh:
+            tl_fh.write(json.dumps(timeleft))
+
+    def tidbyt(self, starlet_file="onair"):
+        """Render and push a tidbyt image"""
+        if not self.options.get('tidbyt_device_id'):
+            return
+        cmd = [PIXLET_PATH, 'render', f'{starlet_file}.star']
+        subprocess.run(cmd, check=True)
+        os.environ['TIDBYT_API_TOKEN'] = self.options.get('tidbyt_api_token')
+        cmd = [
+            PIXLET_PATH,
+            'push',
+            '--installation-id',
+            'radioblue',
+            self.options.get('tidbyt_device_id'),
+            f'{starlet_file}.webp']
+        subprocess.run(cmd, check=True)
+
+    def stop_ah(self):
+        cmd = [
+            'open',
+            'stop.ahcommand',
+        ]
+        subprocess.run(cmd)
+
+    def start_ah(self):
+        cmd = [
+            'open',
+            'start.ahcommand'
+        ]
+        subprocess.run(cmd)
+
+    def stop(self):
+        """Stop client from playing"""
+        self.client.pause()
+
+
+def web():
+    """Run web server"""
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5050)
+
+
+def tidbyt(rbq):
+    """Refresh the tidbyt"""
+    while True:
+        if not rbq.ready:
+            time.sleep(1)
+            continue
+        try:
+            rbq.tidbyt('nowplaying')
+        except Exception:
+            pass
+        time.sleep(1)
+
+
+@app.route("/")
+def timeleft():
+    with open('./timeleft.json', 'r', encoding='utf-8') as tl_fh:
+        try:
+            data = json.loads(tl_fh.read())
+        except json.decoder.JSONDecodeError:
+            data = {} 
+    return jsonify(data)
+
+
+def update_status(rbq):
+    """Update status"""
+    while True:
+        if not rbq.ready:
+            time.sleep(1)
+            continue
+        rbq.update_stats()
+        time.sleep(0.5)
+
 
 def main():
     """Main"""
     rbq = RadioBlueQueue()
     rbq.setup()
-
+    rbq.tidbyt('onair')
+    rbq.start_ah()
     rbq.play()
-    while True:
-        logging.getLogger("plexapi").setLevel(logging.INFO)
-        rbq.sync_playlist()
-        rbq.update_now_playing()
-        rbq.update_time_remaining()
+    web_thread = threading.Thread(target=web, daemon=True).start()
+    tidbyt_thread = threading.Thread(target=tidbyt, args=(rbq,), daemon=True).start()
+    update_thread = threading.Thread(target=update_status, args=(rbq,), daemon=True).start()
+    try:
+        while True:
+            logging.getLogger("plexapi").setLevel(logging.INFO)
+            rbq.refresh_play_queue_from_server()
+            rbq.sync_playlist()
+            rbq.update_now_playing()
+            rbq.ready = True
+            time.sleep(1)
+    except KeyboardInterrupt:
+        LOG.info("Keyboard interrupt, shutting down")
+        rbq.stop_ah()
         time.sleep(1)
+        rbq.tidbyt('offair')
+        rbq.stop()
+        sys.exit()
 
 if __name__ == '__main__':
     main()
